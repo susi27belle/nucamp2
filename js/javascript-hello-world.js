53214ce7861af524b860459be589bbe91c197a1b
susi yanti gultom <susiyantigultom25@gmail.com>
2023-09-02 22:19:57 +0700

    update

---
+++ js/javascript-hello-world.js
@@ -0,0 +1,8 @@
+function getName() {
+    const username = prompt('What is is your name?');   
+    sayHello(username);
+}
+ function sayHello(name) {
+     alert('Hello ' + name + '!');
+ 
+}              
